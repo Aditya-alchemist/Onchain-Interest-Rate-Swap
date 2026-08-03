ab39bbd51ea197456c67edbbec146153f33a0ef8
Aditya41205 <157035985+Aditya41205@users.noreply.github.com>
2026-08-04 00:29:04 +0530

    lending pool test

--- smart-contracts/test/lending/LendingPool.t.sol
+++ smart-contracts/test/lending/LendingPool.t.sol
@@ -9,113 +9,126 @@ import {InterestRateModel} from "../../src/lending/InterestRateModel.sol";
 import {LendingPool} from "../../src/lending/LendingPool.sol";
 
 contract LendingPoolTest is Test {
-    MockUSDC usdc;
-    Governance gov;
-    InterestRateModel model;
-    LendingPool pool;
+MockUSDC usdc;
+Governance gov;
+InterestRateModel model;
+LendingPool pool;
 
-    address alice = address(0xA11CE);
-    address loanManager = address(0xBEEF);
 
-    function setUp() public {
-        usdc = new MockUSDC(address(this));
-        gov = new Governance(address(this), address(this));
-        model = new InterestRateModel(address(this));
+address alice = address(0xA11CE);
+address loanManager = address(0xBEEF);
 
-        pool = new LendingPool(
-            address(usdc),
-            address(model),
-            address(gov),
-            address(this)
-        );
+function setUp() public {
+    usdc = new MockUSDC(address(this));
+    gov = new Governance(address(this), address(this));
+    model = new InterestRateModel(address(this));
 
-        pool.setLoanManager(loanManager);
+    pool = new LendingPool(
+        address(usdc),
+        address(model),
+        address(gov),
+        address(this)
+    );
 
-        usdc.mint(alice, 100_000e6);
+    pool.setLoanManager(loanManager);
 
-        vm.prank(alice);
-        usdc.approve(address(pool), type(uint256).max);
-    }
+    usdc.mint(alice, 100_000e6);
 
-    function testDepositUSDC() public {
-        vm.prank(alice);
-        pool.deposit(10_000e6);
+    vm.prank(alice);
+    usdc.approve(address(pool), type(uint256).max);
+}
 
-        assertEq(pool.totalDeposits(), 10_000e6);
-        assertEq(pool.deposits(alice), 10_000e6);
-        assertEq(usdc.balanceOf(address(pool)), 10_000e6);
-    }
+function testDepositUSDC() public {
+    vm.prank(alice);
+    pool.deposit(10_000e6);
 
-    function testWithdrawUSDC() public {
-        vm.prank(alice);
-        pool.deposit(10_000e6);
+    assertEq(pool.totalDeposits(), 10_000e6);
+    assertEq(pool.deposits(alice), 10_000e6);
+    assertEq(usdc.balanceOf(address(pool)), 10_000e6);
+}
 
-        vm.prank(alice);
-        pool.withdraw(4_000e6);
+function testWithdrawUSDC() public {
+    vm.prank(alice);
+    pool.deposit(10_000e6);
 
-        assertEq(pool.totalDeposits(), 6_000e6);
-        assertEq(pool.deposits(alice), 6_000e6);
-        assertEq(usdc.balanceOf(alice), 94_000e6);
-    }
+    vm.prank(alice);
+    pool.withdraw(4_000e6);
 
-    function testIssueLoan() public {
-        vm.prank(alice);
-        pool.deposit(20_000e6);
+    assertEq(pool.totalDeposits(), 6_000e6);
+    assertEq(pool.deposits(alice), 6_000e6);
+    assertEq(usdc.balanceOf(alice), 94_000e6);
+}
 
-        vm.prank(loanManager);
-        pool.issueLoan(alice, 5_000e6);
+function testIssueLoan() public {
+    vm.prank(alice);
+    pool.deposit(20_000e6);
 
-        assertEq(pool.totalBorrows(), 5_000e6);
-        assertEq(usdc.balanceOf(alice), 85_000e6);
-    }
+    vm.prank(loanManager);
+    pool.issueLoan(alice, 5_000e6);
 
-    function testReceiveRepayment() public {
-        vm.prank(alice);
-        pool.deposit(20_000e6);
+    assertEq(pool.totalBorrows(), 5_000e6);
+    assertEq(usdc.balanceOf(alice), 85_000e6);
+}
 
-        vm.prank(loanManager);
-        pool.issueLoan(alice, 5_000e6);
+function testReceiveRepayment() public {
+    vm.prank(alice);
+    pool.deposit(20_000e6);
 
-        vm.startPrank(alice);
-        usdc.approve(address(pool), 5_000e6);
-        vm.stopPrank();
+    vm.prank(loanManager);
+    pool.issueLoan(alice, 5_000e6);
 
-        vm.prank(loanManager);
-        pool.receiveRepayment(alice, 5_000e6);
+    // LoanManager (payer) must hold tokens and approve the pool.
+    address payer = address(0xCAFE);
 
-        assertEq(pool.totalBorrows(), 0);
-        assertEq(usdc.balanceOf(address(pool)), 20_000e6);
-    }
+    usdc.mint(payer, 5_000e6);
 
-    function testCannotWithdrawMoreThanBalance() public {
-        vm.prank(alice);
-        pool.deposit(1_000e6);
+    vm.startPrank(payer);
+    usdc.approve(address(pool), type(uint256).max);
+    vm.stopPrank();
 
-        vm.prank(alice);
-        vm.expectRevert();
+    vm.prank(loanManager);
+    pool.receiveRepayment(
+        payer,
+        alice,
+        5_000e6,
+        0
+    );
 
-        pool.withdraw(2_000e6);
-    }
+    assertEq(pool.totalBorrows(), 0);
+    assertEq(usdc.balanceOf(address(pool)), 20_000e6);
+}
 
-    function testOnlyLoanManagerCanIssueLoan() public {
-        vm.prank(alice);
-        pool.deposit(10_000e6);
+function testCannotWithdrawMoreThanBalance() public {
+    vm.prank(alice);
+    pool.deposit(1_000e6);
 
-        vm.prank(alice);
-        vm.expectRevert();
+    vm.prank(alice);
+    vm.expectRevert();
 
-        pool.issueLoan(alice, 1_000e6);
-    }
+    pool.withdraw(2_000e6);
+}
 
-    function testCurrentBorrowRate() public {
-        vm.prank(alice);
-        pool.deposit(100_000e6);
+function testOnlyLoanManagerCanIssueLoan() public {
+    vm.prank(alice);
+    pool.deposit(10_000e6);
 
-        vm.prank(loanManager);
-        pool.issueLoan(alice, 50_000e6);
+    vm.prank(alice);
+    vm.expectRevert();
 
-        uint256 rate = pool.currentBorrowRateBps();
+    pool.issueLoan(alice, 1_000e6);
+}
 
-        assertEq(rate, 700);
-    }
+function testCurrentBorrowRate() public {
+    vm.prank(alice);
+    pool.deposit(100_000e6);
+
+    vm.prank(loanManager);
+    pool.issueLoan(alice, 50_000e6);
+
+    uint256 rate = pool.currentBorrowRateBps();
+
+    assertEq(rate, 700);
+}
+
+
 }
