f4f01023734ee7d24c52128be514f7125aa739af
Aditya41205 <157035985+Aditya41205@users.noreply.github.com>
2026-08-19 00:25:55 +0530

    liquidation engine

--- smart-contracts/src/liquidation/LiquidationEngine.sol
+++ smart-contracts/src/liquidation/LiquidationEngine.sol
@@ -1,10 +1,97 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract LiquidationEngine {
-    event Liquidated(uint256 indexed loanId, address indexed liquidator);
+import {Ownable} from "../../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-    function liquidate(uint256 loanId) external {
-        emit Liquidated(loanId, msg.sender);
+interface ILoanManagerLiquidation {
+    function liquidate(
+        uint256 tokenId,
+        address liquidator
+    ) external;
+
+    function isLiquidatable(
+        uint256 tokenId
+    ) external view returns (bool);
+}
+
+contract LiquidationEngine is Ownable {
+    ILoanManagerLiquidation public loanManager;
+
+    event Liquidated(
+        uint256 indexed loanId,
+        address indexed liquidator
+    );
+
+    event LoanManagerUpdated(
+        address indexed loanManager
+    );
+
+    error InvalidAddress();
+    error NotLiquidatable();
+
+    constructor(
+        address initialOwner,
+        address loanManagerAddress
+    )
+        Ownable(initialOwner)
+    {
+        if (
+            loanManagerAddress ==
+            address(0)
+        ) {
+            revert InvalidAddress();
+        }
+
+        loanManager =
+            ILoanManagerLiquidation(
+                loanManagerAddress
+            );
+    }
+
+    function setLoanManager(
+        address loanManagerAddress
+    )
+        external
+        onlyOwner
+    {
+        if (
+            loanManagerAddress ==
+            address(0)
+        ) {
+            revert InvalidAddress();
+        }
+
+        loanManager =
+            ILoanManagerLiquidation(
+                loanManagerAddress
+            );
+
+        emit LoanManagerUpdated(
+            loanManagerAddress
+        );
+    }
+
+    function liquidate(
+        uint256 loanId
+    )
+        external
+    {
+        if (
+            !loanManager.isLiquidatable(
+                loanId
+            )
+        ) {
+            revert NotLiquidatable();
+        }
+
+        loanManager.liquidate(
+            loanId,
+            msg.sender
+        );
+
+        emit Liquidated(
+            loanId,
+            msg.sender
+        );
     }
 }
